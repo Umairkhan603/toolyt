712ed9d9fdb1da17e73ab2308416bd8c392bd9f6
Muhammad Umair Khan <umairnutkani603@gmail.com>
2026-09-18 02:10:51 +0500

    Use Rollout DATABASE_URL in production

--- config/settings/production.py
+++ config/settings/production.py
@@ -7,7 +7,24 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 # PostgreSQL
-if os.getenv('DATABASE_URL') or os.getenv('POSTGRES_DB'):
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    from urllib.parse import urlparse
+
+    db_url = urlparse(DATABASE_URL)
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': db_url.path.lstrip('/'),
+            'USER': db_url.username,
+            'PASSWORD': db_url.password,
+            'HOST': db_url.hostname,
+            'PORT': db_url.port or '5432',
+        }
+    }
+elif os.getenv('POSTGRES_DB'):
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
